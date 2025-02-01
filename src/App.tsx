18cda61dfd6bbf0707d7c9d93d6c899d56d187d8
artursklezovics <klezovich@gmail.com>
2025-02-01 19:05:26 +0100

    Add another example

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,36 @@
-import { useState } from 'react'
+import {useState} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const BlackBox = ({children}) => {
+    return <div className={"border border-black"}>{children}</div>
+}
 
-  return (
-    <>
-      <div className={"text-red-500"}>Hi</div>
-      <div className={"text-orange-100"}>Bye!</div>
-    </>
-  )
+function App() {
+    const [count, setCount] = useState(0)
+
+    return (
+        <>
+            <div className={"text-red-500"}>Hi</div>
+            <div className={"text-orange-100"}>Bye!</div>
+
+            <div className={"columns-3 border border-black"}>
+                <div className={"border border-red-500"}>1</div>
+                <div className={"border border-red-500"}>2</div>
+                <div className={"border border-red-500"}>3</div>
+                {/*<div className={"border border-red-500"}>4</div>*/}
+            </div>
+
+            <div>
+                <div className={"mb-1"}>
+                    <BlackBox>1</BlackBox>
+                </div>
+                <BlackBox>2</BlackBox>
+                <BlackBox>3</BlackBox>
+            </div>
+        </>
+    )
 }
 
 export default App
